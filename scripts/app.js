7e2071f5325d867ca34b9a0700c3a031ad820ff1
kkarim7 <khaleel.karim@gmail.com>
2020-05-24 22:57:06 -0700

    Add files via upload
    Adds search functionality to search for a character based on it's id.

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,6 @@
 let i = 1;
-fetch("https://www.anapioficeandfire.com/api/characters/1")
-  .then((response) => response.json())
-  .then((data) => {
-    document.getElementById("characterName").innerHTML = data.name
-      ? data.name
-      : data.aliases;
-    document.getElementById(
-      "characterData"
-    ).innerHTML = `<h3>Aliases: ${data.aliases}</h3><br/>
-    <h3>Gender: ${data.gender}</h3><br/>
-    <h3>Culture: ${data.culture}</h3><br/>
-    <h3>Played By: ${data.playedBy}</h3><br/>`;
-  });
 
 const fetchURL = (x) => {
-
   let URL = "https://www.anapioficeandfire.com/api/characters/" + x;
 
   fetch(URL)
@@ -39,3 +25,27 @@ nextCharacterBtn.addEventListener("click", () => {
   fetchURL(i);
   console.log(i);
 });
+
+const prevCharacterBtn = document.getElementById("prevChar");
+
+prevCharacterBtn.addEventListener("click", () => {
+  if (i === 1) {
+    alert("Can't go lower than 1");
+    return;
+  }
+  i -= 1;
+  fetchURL(i);
+  console.log(i);
+});
+
+document.getElementById("selectCharBtn").addEventListener("click", () => {
+  const selectCharInput = prompt("Please insert a character id", "1");
+  if (isNaN(selectCharInput) || selectCharInput === null) {
+    return;
+  }
+  fetchURL(selectCharInput);
+  i = parseInt(selectCharInput);
+});
+
+window.onload = fetchURL(i);
+
